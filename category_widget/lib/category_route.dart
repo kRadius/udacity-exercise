9d8b28b5ee5fc9fafe3669627b2b0e9ffda2934e
zhangxinfa <zhangxinfa@51haoyayi.com>
2019-12-01 17:26:17 +0800

    feat: exercise_02_category_route

--- category_widget/lib/category_route.dart
+++ category_widget/lib/category_route.dart
@@ -1,11 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:category_widget/category_widget.dart';
 
+final _backgroundColor = Colors.green[100];
+
+/// Category Route (screen).
+///
+/// This is the 'home' screen of the Unit Converter. It shows a header and
+/// a list of [Categories].
+///
+/// While it is named CategoryRoute, a more apt name would be CategoryScreen,
+/// because it is responsible for the UI at the route's destination.
 class CategoryRoute extends StatelessWidget {
-  c
+  const CategoryRoute();
+
+  static const _categoryNames = <String>[
+    'Length',
+    'Area',
+    'Volume',
+    'Mass',
+    'Time',
+    'Digital Storage',
+    'Energy',
+    'Currency',
+  ];
+
+  static const _baseColors = <Color>[
+    Colors.teal,
+    Colors.orange,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.purpleAccent,
+    Colors.red,
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+
+    final listView = Container(
+      color: _backgroundColor,
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: ListView.builder(
+        padding: EdgeInsets.all(8.0),
+        itemCount: _categoryNames.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 80,
+            child: CategoryWidget(
+                color: _baseColors[index],
+                icon: Icons.cake,
+                name: _categoryNames[index]),
+          );
+        },
+      ),
+    );
+
+    final appBar = AppBar(
+      elevation: 10.0,
+      title: Text(
+        'Unit Converter',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 30.0,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: _backgroundColor,
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: listView,
+    );
   }
 }
--- category_widget/lib/main.dart
+++ category_widget/lib/main.dart
@@ -1,14 +1,6 @@
+import 'package:category_widget/category_route.dart';
 import 'package:category_widget/category_widget.dart';
 import 'package:flutter/material.dart';
-
-//class CategoryWidget extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Center(
-//        child: CategoryWidget(
-//            icon: Icons.cake, color: Colors.redAccent, name: 'RandallStevens'));
-//  }
-//}
 
 final List<Map> entries = <Map>[
   {
@@ -16,23 +8,22 @@ final List<Map> entries = <Map>[
     'onClick': (BuildContext context, int index) {
       Navigator.push(context, MaterialPageRoute<void>(
         builder: (BuildContext context) {
-          return Scaffold(
-            appBar: AppBar(title: Text(entries[index]['name'])),
-            body: Center(
-              child: CategoryWidget(
-                  icon: Icons.cake,
-                  color: Colors.redAccent,
-                  name: 'RandallStevens'),
-            ),
-          );
+          return CategoryWidget(
+              icon: Icons.cake,
+              color: Colors.redAccent,
+              name: 'RandallStevens');
         },
       ));
     }
   },
   {
     'name': 'CategoryRoute',
-    'onClick': (BuildContext context) {
-      print('click');
+    'onClick': (BuildContext context, int index) {
+      Navigator.push(context, MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return CategoryRoute();
+        },
+      ));
     }
   },
 ];
@@ -43,7 +34,10 @@ void main() {
         debugShowCheckedModeBanner: true,
         home: Scaffold(
             appBar: AppBar(
-              title: Text('Udacity Exercise', style: TextStyle(fontSize: 30),),
+              title: Text(
+                'Udacity Exercise',
+                style: TextStyle(fontSize: 30),
+              ),
             ),
             body: ListView.separated(
               padding: EdgeInsets.all(8.0),
